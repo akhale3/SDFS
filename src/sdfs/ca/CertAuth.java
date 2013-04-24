2d4c75e65d28d1c121d1d8c155bb900261e51bb6
anishkhale <anish_1170@yahoo.com>
2013-04-24 14:31:40 -0400

    042420131431: Client-Server connection established

--- src/sdfs/ca/CertAuth.java
+++ src/sdfs/ca/CertAuth.java
@@ -3,5 +3,20 @@ package sdfs.ca;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class CertAuth {
+	
+	void generateCert()
+	{
+		
+	}
+	
+	void issueCert()
+	{
+		
+	}
+	
+	void revokeCert()
+	{
+		
+	}
 
 }
---
+++ src/sdfs/server/SDFSServer.java
@@ -0,0 +1,26 @@
+package sdfs.server;
+
+import java.io.*;
+import java.net.*;
+
+public class SDFSServer {
+
+   public static void main(String args[]) throws Exception
+      {
+         String clientSentence;
+         String capitalizedSentence;
+         ServerSocket welcomeSocket = new ServerSocket(6789);
+
+         while(true)
+         {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient =
+               new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+         }
+      }
+}
